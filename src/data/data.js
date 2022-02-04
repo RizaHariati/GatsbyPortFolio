3922cf127dfb57751e48da3a4655e1fddce066ae
Riza Hariati <76550345+RizaHariati@users.noreply.github.com>
2022-02-04 07:47:22 +0700

    fixing some typos

--- src/data/data.js
+++ src/data/data.js
@@ -42,8 +42,8 @@ const projects = [
     title: "Azriblog",
     imageLink: "Web4",
     text: [
-      "I always thought Laravel is only used for back-end side of development. But after learning from a very professional, very thorouugh Web Programming UNPAS tutorial on Laravel, I realized that it's actually very good for a simple front-end side of web development ",
-      "Based on that tutorial, I created this blog website, with the help of the good old bootstrap and MySQL, show casing my laravel implementation on routing, templating HTML, and authentication",
+      "I always thought Laravel is only used for back-end side of development. But after learning from a very professional, very thorough Web Programming UNPAS tutorial on Laravel, I realized that it's actually very good for a simple front-end side of web development ",
+      "Based on that tutorial, I created this blog website, with the help of the good old Bootstrap and MySQL, show casing my laravel implementation on routing, templating HTML, and authentication",
 
     ],
     images: ["azriblog-website", "azriblog-tablet", "azriblog-phone"],
